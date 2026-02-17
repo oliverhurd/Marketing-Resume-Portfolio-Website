93524ae133b3026d769fdfb20d1dbceb3c3b7dca
Oliver <hurdtechbangkok@gmail.com>
2026-02-17 10:55:53 +0700

    Update App.tsx
    feb17

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,41 @@ import { HomePage } from './pages/HomePage';
 import { ResumePage } from './pages/ResumePage';
 import { CaseStudiesPage } from './pages/CaseStudiesPage';
 import { CaseStudyDetailPage } from './pages/CaseStudyDetailPage';
+import { ShoppingDoneSmartDetailPage } from './pages/ShoppingDoneSmartDetailPage';
+import { TookjaiReviewDetailPage } from './pages/TookjaiReviewDetailPage';
 import { BlogPage } from './pages/BlogPage';
 import { NotesPage } from './pages/NotesPage';
 import { ContactPage } from './pages/ContactPage';
+
 function AnimatedRoutes() {
   const location = useLocation();
-  return <AnimatePresence mode="wait">
+  return (
+    <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
         <Route path="/resume" element={<ResumePage />} />
         <Route path="/case-studies" element={<CaseStudiesPage />} />
         <Route path="/case-studies/braveheart-trading" element={<CaseStudyDetailPage />} />
+        <Route path="/case-studies/shopping-done-smart" element={<ShoppingDoneSmartDetailPage />} />
+        <Route path="/case-studies/tookjai-review" element={<TookjaiReviewDetailPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/notes" element={<NotesPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </AnimatePresence>;
+    </AnimatePresence>
+  );
 }
+
 export function App() {
-  return <BrowserRouter>
-      <div className="min-h-screen bg-surface flex flex-col">
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen w-full flex flex-col">
         <Header />
-        <div className="flex-1">
+        <main className="flex-1">
           <AnimatedRoutes />
-        </div>
+        </main>
         <Footer />
       </div>
-    </BrowserRouter>;
+    </BrowserRouter>
+  );
 }
